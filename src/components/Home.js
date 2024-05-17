bc61b3d8c6d8c3652f481e80577350dbb813ea61
sanjay892000 <sanjay892000@gmail.com>
2024-05-17 13:14:50 +0530

    repo added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import { Button } from '@mui/material';
 import '../style/home.css'
 
 function Home() {
     const githubUri = "https://api.github.com/users/";
-
+    const [userRepo, setuserRepo] = useState('');
     const [userName, setuserName] = useState('');
     const [userDetails, setUserDetails] = useState('');
 
@@ -12,19 +14,23 @@ function Home() {
         const user = await response.json();
         console.log(user);
         setUserDetails(user)
-        /*  const repos = await fetch(user.repos_url);
-         const repo = await repos.json();
-         console.log(repo); */
+    };
+
+    const getGithubRepos = async () => {
+        const repos = await fetch(userDetails.repos_url);
+        const repo = await repos.json();
+        console.log(repo);
+        setuserRepo(repo);
     }
 
     return (
         <div className="container">
             <div className="heading">
-            <h2>Enter GitHub User Name </h2>
+                <h2>Enter GitHub User Name </h2>
             </div>
             <div className="my-3 search-user">
                 <img src='./github-logo.png' alt="loading..." /><input type="text" className="form-control mx-2" id="getGithubUser" placeholder="Enter github user name" onChange={(e) => { setuserName(e.target.value) }} />
-                <button type="button" className="btn btn-primary" onClick={getGithubUser}>Enter</button>
+                <Button variant="contained" color="secondary" className="buttonc mx-10" style={{ color: "White", textTransform: "none", fontFamily: "'Poppins', sans-serif", fontSize: "1.3rem" }} onClick={getGithubUser}>Enter</Button>
             </div>
 
             {userDetails ? <div className="profile">
@@ -37,7 +43,7 @@ function Home() {
                     <div className="user-location"><h1>Location: <span>{userDetails.location}</span></h1> </div>
                     <div className="user-url"><h1>GitHub url: <span> <a href={userDetails.html_url}>{userDetails.html_url}</a></span></h1> </div>
                     <div className="user-content mt-2">
-                        <button type='button'>Repository({userDetails.public_repos})</button>
+                        <button type='button' onClick={getGithubRepos}>Repository({userDetails.public_repos})</button>
                         <button type='button'>Followers({userDetails.followers})</button>
                         <button type='button'>Following({userDetails.following})</button>
                     </div>
@@ -47,4 +53,4 @@ function Home() {
     )
 }
 
-export default Home
+export default Home;
--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Button } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Link } from "react-router-dom";
 import '../style/repository.css'
 
 function Repository() {
   return (
     <div className='container'>
-      
+      <Button className="my-4" variant="text" color="secondary" startIcon={<ArrowBackIcon />} component={Link} to="/" style={{ textTransform: "none", fontFamily: "'Poppins', sans-serif" }}>Home page</Button>
     </div>
   )
 }
